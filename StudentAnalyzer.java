b072f812da87d975be0c38dc9cc69a25bae6a984
Safura-12G3 <safurasaif9g3@gmail.com>
2026-04-13 21:33:04 +0400

    Created StudentAnalyzer with ArrayList

---
+++ StudentAnalyzer.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class StudentAnalyzer {
+    private ArrayList<Student> studentList;
+
+    public StudentAnalyzer(ArrayList<Student> list) {
+        studentList = list;
+    }
+
+}
+
