a47fee6dabcfe17363529cdc15227bd5fbebe7ad
Safura-12G3 <safurasaif9g3@gmail.com>
2026-04-13 22:05:20 +0400

    Part (b) - Safe Removal During Traversal
    Implemented removeFaling method

--- StudentAnalyzer.java
+++ StudentAnalyzer.java
@@ -7,7 +7,7 @@ public class StudentAnalyzer {
         studentList = list;
     }
 
-    // Part (a): Conditional Counting (countAbove)
+    // Part (a) - Conditional Counting (countAbove)
     public int countAbove(int threshold) {
         int count = 0;
         for (Student s : studentList) {
@@ -17,6 +17,18 @@ public class StudentAnalyzer {
         }
         return count;
 
+    }
+
+    // Part (b) - Safe Removal During Traversal (removeFalling)
+    public void removeFalling(int passMark) {
+        int i = 0;
+        while (i < studentList.size()) {
+            if (studentList.get(i).getMarks() < passMark) {
+                studentList.remove(i);
+            } else {
+                i++;
+            }
+        }
     }
 
 }
