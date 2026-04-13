bc6067d9400d3844aad4357142f5f9c55f80746e
Safura-12G3 <safurasaif9g3@gmail.com>
2026-04-13 21:35:56 +0400

    Part (a) - Conditional Counting
    Implemented countAbove method

--- StudentAnalyzer.java
+++ StudentAnalyzer.java
@@ -7,5 +7,16 @@ public class StudentAnalyzer {
         studentList = list;
     }
 
-}
+    // Part (a): Conditional Counting (countAbove)
+    public int countAbove(int threshold) {
+        int count = 0;
+        for (Student s : studentList) {
+            if (s.getMarks() > threshold) {
+                count++;
+            }
+        }
+        return count;
 
+    }
+
+}
